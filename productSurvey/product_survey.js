f2d67aca9ea54e37f5680ffd45d69911bc590504
ANUJCHOUBEY14 <anujchoubey190@gmail.com>
2024-08-13 06:23:44 -0400

    message

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,7 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-
+    const UserExperience = document.getElementById('userExperinceText').value;
 
  
     alert('Thank you for your valuable feedback')
@@ -17,7 +17,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-
+    document.getElementById('userExperince').innerHTML = UserExperience;
     document.getElementById('userInfo').style.display = 'block';
 }
 const submitButton=document.getElementById('submitBtn');
